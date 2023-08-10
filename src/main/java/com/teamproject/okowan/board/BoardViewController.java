3941953b628b62dc933d240e309bece0c2e8276d
letsgo-nation <ssowon528@gmail.com>
2023-08-10 16:10:51 +0900

    pr test2

--- src/main/java/com/teamproject/okowan/board/BoardViewController.java
+++ src/main/java/com/teamproject/okowan/board/BoardViewController.java
@@ -5,16 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/okw/view/users")
+@RequestMapping("/okw/view")
 public class BoardViewController {
 
-    @GetMapping("/login-signup")
-    public String loginSignup() {
-        return "login-signup";
-    }
-
-    @GetMapping("/profile")
-    public String profile() {
-        return "profile";
-    }
 }
--- src/main/java/com/teamproject/okowan/user/UserViewController.java
+++ src/main/java/com/teamproject/okowan/user/UserViewController.java
@@ -6,7 +6,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/okw/view/boards")
+@RequestMapping("/okw/view/users")
 public class UserViewController {
 
+    @GetMapping("/login-signup")
+    public String board() {
+        return "login-signup";
+    }
+
+    @GetMapping("/profile")
+    public String profile() {
+        return "profile";
+    }
 }
