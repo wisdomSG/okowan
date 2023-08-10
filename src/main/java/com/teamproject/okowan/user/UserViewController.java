ee9d938c3553cc21f44caf8709724a6aa379d071
letsgo-nation <ssowon528@gmail.com>
2023-08-10 16:05:11 +0900

    pr test

--- src/main/java/com/teamproject/okowan/user/UserViewController.java
+++ src/main/java/com/teamproject/okowan/user/UserViewController.java
@@ -6,16 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/okw/view/users")
+@RequestMapping("/okw/view/boards")
 public class UserViewController {
 
-    @GetMapping("/login-signup")
-    public String loginSignup() {
-        return "login-signup";
-    }
-
-    @GetMapping("/profile")
-    public String profile() {
-        return "profile";
-    }
 }
