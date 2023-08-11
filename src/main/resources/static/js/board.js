a9281165ed0eff469e3f0ad41a3dbc98ab56d69e
letsgo-nation <ssowon528@gmail.com>
2023-08-11 16:41:55 +0900

    보드 리스트 조회 최신

---
+++ src/main/resources/static/js/board.js
@@ -0,0 +1,73 @@
+// 보드 리스트
+document.addEventListener("DOMContentLoaded", function () {
+    const token = Cookies.get('Authorization');
+
+    $.ajax({
+        type: "GET",
+        url: "/okw/boards",
+        headers: {'Authorization': token}
+            .done(function (response) {
+                console.log(response);
+                // $('#post-cards').empty();
+                for (let i = 0; i < response.length; i++) {
+                    console.log(response);
+                    // let boardTitle = response[i]['title'];
+                    // let post_id = response[i]['id'];
+                    // setHtml(post_title, post_id);
+                }
+            })
+            .fail(function () {
+                // window.location.href = "/okw/view/users/login-signup";
+            })
+    })
+})
+
+
+// 보드 추가 모달창
+document.getElementById("openModalBtn").addEventListener("click", function () {
+    var modal = document.getElementById("myModal");
+    modal.style.display = "block";
+});
+
+document.querySelector(".close").addEventListener("click", function () {
+    var modal = document.getElementById("myModal");
+    modal.style.display = "none";
+});
+
+// document.getElementById("cancelBtn").addEventListener("click", function() {
+//     var modal = document.getElementById("myModal");
+//     modal.style.display = "none";
+// });
+
+document.getElementById("confirmBtn").addEventListener("click", postBoard);
+
+function postBoard() {
+    const token = Cookies.get('Authorization'); // 쿠키에서 토큰 가져오기
+    const title = document.querySelector("#title").value;
+    const description = document.querySelector("#description").value;
+
+    // 입력하는 데이터
+    const data = {
+        title: title,
+        description: description
+    }
+
+    $.ajax({
+        type: 'POST',
+        url: '/okw/boards/post',
+        headers: {'Authorization': token}, // 헤더에 토큰 주입
+        contentType: "application/json", // 읽는 타입이 JSON이라는 뜻
+        data: JSON.stringify(data), // JAVA -> JSON
+
+        success: function (response) {
+            alert("보드 추가 성공");
+            // window.location.href = "/okw/view/boards/board";
+        }, error: function (req, status, error) {
+            alert("보드 추가 실패");
+            console.log(req, status, error);
+            // window.location.href = "/okw/view/boards/board";
+        }
+    })
+}
+
+// 보드 추가 모달창 끝
